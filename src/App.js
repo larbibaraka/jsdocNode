d0ba2fa182f4ce9ebdf8e9c76bff660355c91e31
larbibaraka <laridev@gmail.com>
2020-02-21 23:06:07 +0100

    adding methods

--- src/App.js
+++ src/App.js
@@ -32,6 +32,21 @@ class Post {
         this.token = token;
     }
 
+    /**
+     * 
+     * @typedef {Posts} Posts
+     * @property {Number} UserId the user id 
+     * @property {Number} id the  id 
+     * @property {String} title the title of the post 
+     * @property {String} body the body of the post
+     */
+
+    /**
+     * List of all posts  for given number
+     * @param {Number} [options.postnumber=10] the post number
+     * @return {Posts[]} posts returned by the Api 
+     */
+
     async posts({postnumber = 10}){
         const maxResult = 100;
         const response = await fetch("https://jsonplaceholder.typicode.com/posts");
