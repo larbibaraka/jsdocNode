fc42be252826a917aa89b724fafc39e28b02c7c1
larbibaraka <laridev@gmail.com>
2020-02-21 22:53:23 +0100

    adding fist docs comments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,29 @@
+/**
+ * Post Client Library
+ * 
+ * @example
+ * const post = new Post({
+ *  host : "yourself",
+ *  user : "heheh@hehe.com",
+ *  token :  "secret",
+ *  version : 2
+ * })
+ * 
+ * const posts =  post.posts()
+ * 
+ * @see  http://www.google.com
+ * @todo  implements all the methods
+ * 
+ */
 class Post {
+    /**
+     * @param {Object} options
+     * @param {String} options.host the host of your app
+     * @param {String} options.user the user of the app
+     * @param {String} options.token token must be given 
+     * @param {Number} options.version the version number
+     * 
+     * */
     constructor({host, user , token, version = 3}){
         this.version = version;
         this.host = host;
