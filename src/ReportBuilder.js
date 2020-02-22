f6414fd2dd3587463cc357223cefe4b225e58109
larbibaraka <laridev@gmail.com>
2020-02-22 12:13:58 +0100

    adding mermaid

--- src/ReportBuilder.js
+++ src/ReportBuilder.js
@@ -2,12 +2,12 @@
  * Report Builder
  * @mermaid
  * sequenceDiagram
- *  participant App
- *  participant Post
- *  App->>Post:posts({postsnumber})
- *  Post->>App:ProjectObject
- *  App->>Post:search()
- *  Post->>App:IssueObject[]
+ *   participant App
+ *   participant Post
+ *   App->>Post:posts({postsnumber})
+ *   Post->>App:ProjectObject
+ *   App->>Post:search()
+ *   Post->>App:IssueObject[]
  */
 class ReportBuilder {
     constructor(args) {
