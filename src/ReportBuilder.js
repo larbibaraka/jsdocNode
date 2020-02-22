7eaca0fb8db31ffa10c397919f1b282766884bf8
larbibaraka <laridev@gmail.com>
2020-02-22 12:26:48 +0100

    adding new code

--- src/ReportBuilder.js
+++ src/ReportBuilder.js
@@ -5,9 +5,11 @@
  * @param {string} author - The author of the book.
  *
  * @mermaid
- *   sequenceDiagram;
- *     Alice->>John: Hello John, how are you?
- *     John-->>Alice: Great!
+ * sequenceDiagram
+ *  Alice->John: Hello John, how are you?
+ *  loop Every minute
+ *      John-->Alice: Great!
+ *  end
  */
 function Book(title, author) {
     /* ... */
